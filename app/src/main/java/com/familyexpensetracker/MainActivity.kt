3d788289f03a5f85ffdfcf1c40ab903b8e9a67a3
Janki Shah <jshah5321@gmail.com>
2025-03-27 13:53:57 +0530

    testing

--- app/src/main/java/com/familyexpensetracker/MainActivity.kt
+++ app/src/main/java/com/familyexpensetracker/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             FamilyExpenseTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
